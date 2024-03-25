ea871e7c5ae0d2f5a3baee9978e63266b140d5fc
Rinaldy Adin <16521390@mahasiswa.itb.ac.id>
2024-03-25 18:58:10 +0700

    add paralelism to pivot calculations

--- src/open-mpi/mpi.c
+++ src/open-mpi/mpi.c
@@ -6,7 +6,7 @@ void printMatrix(double *mat, int n) {
     printf("%d\n", n);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            printf("%f ", mat[i * n + j]);
+            printf("%lf ", mat[i * n + j]);
         }
         printf("\n");
     }
@@ -24,12 +24,6 @@ void readMatrixFromFile(char *filename, double **mat, int *n, int rank, int size
 
     if (rank == 0) {
         fscanf(file, "%d", n);
-        if (*n % size != 0) {
-            printf("Number of rows must be divisible by the number of processes.\n");
-            fclose(file);
-            MPI_Finalize();
-            exit(1);
-        }
 
         *mat = (double *)malloc((*n) * (*n) * sizeof(double));
         for (int i = 0; i < (*n) * (*n); i++) {
@@ -39,41 +33,51 @@ void readMatrixFromFile(char *filename, double **mat, int *n, int rank, int size
     }
 }
 
-void matrix_inversion(double *mat, int n) {
+void matrix_inversion(double *mat, int n, int local_n, int start_row, int end_row) {
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    int start = rank * n / size;
-    int end = (rank + 1) * n / size;
-
-    double *identity = (double *)malloc(n * n * sizeof(double));
-    for (int i = start; i < end; i++) {
+    double *identity = (double *)malloc(local_n * n * sizeof(double));
+    for (int i = start_row; i < end_row; i++) {
         for (int j = 0; j < n; j++) {
-            identity[i * n + j] = (i == j) ? 1.0 : 0.0;
+            identity[(i - start_row) * n + j] = (i == j) ? 1.0 : 0.0;
         }
     }
 
     // Gauss-Jordan
+    double *pivot_mat = (double *)malloc(n * sizeof(double));
+    double *pivot_ident = (double *)malloc(n * sizeof(double));
     for (int i = 0; i < n; i++) {
-        double divisor = mat[i * n + i];
-        for (int j = 0; j < n; j++) {
-            mat[i * n + j] /= divisor;
-            identity[i * n + j] /= divisor;
+        int pivot_process = i * size / n;
+        int local_pivot = i - start_row;
+
+        if (rank == pivot_process) {
+            double divisor = mat[local_pivot * n + i];
+            for (int j = 0; j < n; j++) {
+                mat[local_pivot * n + j] /= divisor;
+                identity[local_pivot * n + j] /= divisor;
+
+                pivot_mat[j] = mat[local_pivot * n + j];
+                pivot_ident[j] = identity[local_pivot * n + j];
+            }
         }
 
-        for (int j = 0; j < n; j++) {
-            if (j != i) {
-                double factor = mat[j * n + i];
-                for (int k = 0; k < n; k++) {
-                    mat[j * n + k] -= factor * mat[i * n + k];
-                    identity[j * n + k] -= factor * identity[i * n + k];
+        MPI_Bcast(pivot_mat, n, MPI_DOUBLE, pivot_process, MPI_COMM_WORLD);
+        MPI_Bcast(pivot_ident, n, MPI_DOUBLE, pivot_process, MPI_COMM_WORLD);
+
+        for (int local_i = 0; local_i < local_n; local_i++) {
+            if (!(rank == pivot_process && local_i == local_pivot)) {
+                double factor = mat[local_i * n + i];
+                for (int j = 0; j < n; j++) {
+                    mat[local_i * n + j] -= factor * pivot_mat[j];
+                    identity[local_i * n + j] -= factor * pivot_ident[j];
                 }
             }
         }
     }
 
-    for (int i = 0; i < n * n; i++) {
+    for (int i = 0; i < local_n * n; i++) {
         mat[i] = identity[i];
     }
 
@@ -98,7 +102,7 @@ void performMatrixOperations(int n, int size, int rank, double *mat) {
     double *local_mat = (double *)malloc(sendcounts[rank] * sizeof(double));
     MPI_Scatterv(mat, sendcounts, displs, MPI_DOUBLE, local_mat, sendcounts[rank], MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-    matrix_inversion(local_mat, sendcounts[rank] / n);
+    matrix_inversion(local_mat, n, sendcounts[rank] / n, displs[rank] / n, (displs[rank] + sendcounts[rank]) / n);
 
     double *result_mat = NULL;
     if (rank == 0) {
